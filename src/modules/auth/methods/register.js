5961168b8b4e549399ea9e431c7bf7c7ee62c6cd
Brandon Clark <yhbrandon@gmail.com>
2016-11-20 15:35:44 -0600

    Being dumb

--- src/modules/auth/methods/register.js
+++ src/modules/auth/methods/register.js
@@ -11,7 +11,7 @@ const register = (req, res, next) => {
 
   return bcryptSalt(10)
     .then(salt => bcryptHash(password, salt, null))
-    .then(hashedPassword => db.none('INSERT INTO ohhi_user(email, password, current_station) values($1, $2)', [email, hashedPassword, current_station]))
+    .then(hashedPassword => db.none('INSERT INTO ohhi_user(email, password, current_station) values($1, $2, $3)', [email, hashedPassword, current_station]))
     .then(() => res.status(200).json({ message: 'Registration successful!', success: true }))
     .catch(error => {
       res.status(400).json({ success: false, message: 'Registration failed!' })
