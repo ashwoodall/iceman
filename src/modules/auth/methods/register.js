4c1846549d1d303f2e785d779b6001ca23d12418
Brandon Clark <yhbrandon@gmail.com>
2016-11-20 15:32:29 -0600

    Adding current_station to auth

--- src/modules/auth/methods/register.js
+++ src/modules/auth/methods/register.js
@@ -7,11 +7,11 @@ const bcryptHash = Promise.promisify(hash)
 const bcryptSalt = Promise.promisify(genSalt)
 
 const register = (req, res, next) => {
-  const { email, password } = req.body
+  const { current_station, email, password } = req.body
 
   return bcryptSalt(10)
     .then(salt => bcryptHash(password, salt, null))
-    .then(hashedPassword => db.none('INSERT INTO ohhi_user(email, password) values($1, $2)', [email, hashedPassword]))
+    .then(hashedPassword => db.none('INSERT INTO ohhi_user(email, password, current_station) values($1, $2)', [email, hashedPassword, current_station]))
     .then(() => res.status(200).json({ message: 'Registration successful!', success: true }))
     .catch(error => {
       res.status(400).json({ success: false, message: 'Registration failed!' })
