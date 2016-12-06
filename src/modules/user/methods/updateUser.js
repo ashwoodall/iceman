5e0b7bb3eb0b22c8354d63e97cc8b1b50d2d69ab
Brandon Clark <yhbrandon@gmail.com>
2016-12-05 22:28:53 -0600

    Setting default

--- src/modules/user/methods/updateUser.js
+++ src/modules/user/methods/updateUser.js
@@ -61,7 +61,7 @@ const updateUser = (req, res, next) => {
   const batchQueries = () => {
     db.tx(transaction => {
       const queries = [
-        transaction.one('UPDATE ohhi_user SET first_name=$1, last_name=$2, birth_date=$3, hometown=$4, profile_picture=$5, introduction=$6, has_kids=$7, has_pets=$8, number_of_kids=$9, about_pets=$10, is_service_member=$11, current_station=$12, facebook=$13, twitter=$14, instagram=$15, pinterest=$16 WHERE id=$17 RETURNING *', [
+        transaction.one('UPDATE ohhi_user SET first_name=$1, last_name=$2, birth_date=$3, hometown=$4, profile_picture=$5, introduction=$6, has_kids=$7, has_pets=$8, number_of_kids=$9, about_pets=$10, is_service_member=$11, current_station=$12, facebook=$13, twitter=$14, instagram=$15, pinterest=$16, completed_profile=$17 WHERE id=$18 RETURNING *', [
           first_name,
           last_name,
           birth_date,
@@ -78,6 +78,7 @@ const updateUser = (req, res, next) => {
           twitter,
           instagram,
           pinterest,
+          completed_profile,
           userId
         ])
       ]
@@ -105,7 +106,7 @@ const updateUser = (req, res, next) => {
   const joinResults = () => {
     return db.tx(transaction => {
       const queries = [
-        transaction.one('SELECT first_name, last_name, birth_date, hometown, profile_picture, introduction, has_kids, has_pets, number_of_kids, about_pets, is_service_member, current_station, facebook, twitter, instagram, pinterest from ohhi_user WHERE id=$1', [userId]),
+        transaction.one('SELECT first_name, last_name, birth_date, hometown, profile_picture, introduction, has_kids, has_pets, number_of_kids, about_pets, is_service_member, current_station, facebook, twitter, instagram, pinterest, completed_profile from ohhi_user WHERE id=$1', [userId]),
         transaction.query('SELECT kids_age_label FROM ohhi_user_kids_age LEFT JOIN ohhi_kids_age ON ohhi_kids_age.id = ohhi_user_kids_age.kids_age_id where ohhi_user_kids_age.user_id=$1', [userId]),
         transaction.query('SELECT activity_label FROM ohhi_user_activity LEFT JOIN ohhi_activity ON ohhi_activity.id = ohhi_user_activity.activity_id where ohhi_user_activity.user_id=$1', [userId])
       ]
