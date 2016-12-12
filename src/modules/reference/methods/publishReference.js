f72684aa2560fe3e033238fef0095fccb29611fb
Brandon Clark <yhbrandon@gmail.com>
2016-12-11 21:45:32 -0600

    Forgot the SET

--- src/modules/reference/methods/publishReference.js
+++ src/modules/reference/methods/publishReference.js
@@ -3,10 +3,10 @@ import db from '../../../core/db'
 const publishReference = (req, res, next) => {
   const { referenceId } = req.params
 
-  db.one('UPDATE ohhi_reference is_published=$1 WHERE ID=$2', [true, referenceId])
+  db.none('UPDATE ohhi_reference SET is_published=$1 WHERE ID=$2', [true, referenceId])
     .then(id => res.status(200).json({ message: 'Reference successfully published!', success: true }))
     .catch(error => {
-      res.status(400).json({ success: false, message: 'Cannot published reference!' })
+      res.status(400).json({ success: false, message: 'Cannot publish reference!' })
 
       return next(error)
     })
