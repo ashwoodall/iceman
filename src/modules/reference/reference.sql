61fd0d0ea866137595e7afd47c3639eea1ed5e68
Brandon Clark <yhbrandon@gmail.com>
2016-12-11 19:24:51 -0600

    Reference sql changes

--- src/modules/reference/reference.sql
+++ src/modules/reference/reference.sql
@@ -3,10 +3,9 @@ CREATE TABLE ohhi_reference(
   id            SERIAL PRIMARY KEY,
   author_id     INTEGER REFERENCES ohhi_user (id),
   recipient_id  INTEGER REFERENCES ohhi_user (id),
-  title         VARCHAR(255),
   body          TEXT,
-  isDeleted     BOOLEAN DEFAULT FALSE,
-  isPublished   BOOLEAN DEFAULT FALSE,
+  is_deleted    BOOLEAN DEFAULT FALSE,
+  is_published  BOOLEAN DEFAULT FALSE,
   timestamp     TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
