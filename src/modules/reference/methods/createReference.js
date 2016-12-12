fdd26f9967c888c0c0876869bd14d0f502c6052f
Brandon Clark <yhbrandon@gmail.com>
2016-12-11 18:34:21 -0600

    Remove title from reference

--- src/modules/reference/methods/createReference.js
+++ src/modules/reference/methods/createReference.js
@@ -2,13 +2,13 @@ import db from '../../../core/db'
 
 const createReference = (req, res, next) => {
   const { id } = req.user
-  const { recipient_id, title, body } = req.body
+  const { recipient_id, body } = req.body
 
   db.one('SELECT * FROM ohhi_reference WHERE author_id=$1 AND recipient_id=$2', [id, recipient_id])
     .then(reference => res.status(400).json({ message: 'Reference already exists', success: false }))
     .catch(error => {
       if (error.received === 0) {
-        db.none('INSERT INTO ohhi_reference(author_id, recipient_id, title, body, isPublished) VALUES($1, $2, $3, $4, $5)', [id, recipient_id, title, body, false])
+        db.none('INSERT INTO ohhi_reference(author_id, recipient_id, body, isPublished) VALUES($1, $2, $3, $4)', [id, recipient_id, body, false])
           .then(() => res.status(200).json({ message: 'Reference created successfully!', success: true }))
           .catch(error => {
             res.status(400).json({ message: 'Cannot create reference!', success: false })
