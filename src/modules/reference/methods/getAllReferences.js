126c2171a679f2f65800f3a3c37ef695e811616f
Brandon Clark <yhbrandon@gmail.com>
2016-12-11 20:58:10 -0600

    Adding user data to reference call

--- src/modules/reference/methods/getAllReferences.js
+++ src/modules/reference/methods/getAllReferences.js
@@ -3,13 +3,22 @@ import db from '../../../core/db'
 const getAllReferences = (req, res, next) => {
   const { id } = req.user
 
-  db.many('SELECT id, author_id, recipient_id, is_published, body FROM ohhi_reference WHERE recipient_id=$1', [id])
-    .then(references => res.status(200).json({ message: 'References found!', success: true, data: references }))
-    .catch(error => {
-      res.status(400).json({ message: 'Cannot find references!', success: false })
+  db.task(tasks => {
+    return tasks.map('SELECT id, author_id, recipient_id, is_published, body FROM ohhi_reference WHERE recipient_id=$1', [id], reference => {
+      return tasks.one('SELECT id, first_name, last_name FROM ohhi_user WHERE id=$1', [id], user => {
+        reference.recipient = user
 
-      return next(error)
+        return reference
+      })
     })
+    .then(tasks.batch)
+  })
+  .then(data => res.status(200).json({ message: 'References found!', success: true, data: data }))
+  .catch(error => {
+    res.status(400).json({ message: 'Cannot find references!', success: false })
+
+    return next(error)
+  })
 }
 
 export default getAllReferences
