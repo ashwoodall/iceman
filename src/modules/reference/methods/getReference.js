40aa3d7b8d0b14bb15232154a263952754b15b3c
Brandon Clark <yhbrandon@gmail.com>
2016-12-11 19:18:06 -0600

    Removing title from single reference

--- src/modules/reference/methods/getReference.js
+++ src/modules/reference/methods/getReference.js
@@ -3,7 +3,7 @@ import db from '../../../core/db'
 const getReference = (req, res, next) => {
   const { referenceId } = req.params
 
-  db.many('SELECT id, author_id, recipient_id, title, body FROM ohhi_reference WHERE id=$1', [referenceId])
+  db.one('SELECT id, author_id, recipient_id, body, isPublished FROM ohhi_reference WHERE id=$1', [referenceId])
     .then(reference => res.status(200).json({ message: 'References found!', success: true, data: reference }))
     .catch(error => {
       res.status(400).json({ message: 'Cannot find references!', success: false })
