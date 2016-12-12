a63e144bd352cd6f545fce2877b185f14ee5240b
Brandon Clark <yhbrandon@gmail.com>
2016-12-11 19:15:51 -0600

    Return published flag

--- src/modules/reference/methods/getAllReferences.js
+++ src/modules/reference/methods/getAllReferences.js
@@ -3,7 +3,7 @@ import db from '../../../core/db'
 const getAllReferences = (req, res, next) => {
   const { id } = req.user
 
-  db.many('SELECT id, author_id, recipient_id, title, body FROM ohhi_reference WHERE recipient_id=$1', [id])
+  db.many('SELECT id, author_id, recipient_id, isPublished, body FROM ohhi_reference WHERE recipient_id=$1', [id])
     .then(references => res.status(200).json({ message: 'References found!', success: true, data: references }))
     .catch(error => {
       res.status(400).json({ message: 'Cannot find references!', success: false })
