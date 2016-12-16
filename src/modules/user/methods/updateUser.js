884a0fbbaa335f7033331579dc0aed23716b36a3
Brandon Clark <yhbrandon@gmail.com>
2016-12-15 22:44:49 -0600

    Return user id after profile save

--- src/modules/user/methods/updateUser.js
+++ src/modules/user/methods/updateUser.js
@@ -1,4 +1,5 @@
 import db, { pgp } from '../../../core/db'
+import { every } from 'lodash'
 import Promise from 'bluebird'
 
 const helpers = pgp.helpers
@@ -23,8 +24,7 @@ const updateUser = (req, res, next) => {
     instagram,
     pinterest,
     kids_ages,
-    activities,
-    completed_profile
+    activities
   } = req.body
 
   let kidsAgeJunctionInsertValues
@@ -60,6 +60,8 @@ const updateUser = (req, res, next) => {
 
   // Queries executed in the same batch/transaction will be rolled back if any one of them fails.
   const batchQueries = () => {
+    let isComplete = completedProfile && activities.length
+
     return db.tx(transaction => {
       const queries = [
         transaction.one('UPDATE ohhi_user SET first_name=$1, last_name=$2, birth_date=$3, hometown=$4, profile_picture=$5, introduction=$6, has_kids=$7, has_pets=$8, number_of_kids=$9, about_pets=$10, is_service_member=$11, current_station=$12, facebook=$13, twitter=$14, instagram=$15, pinterest=$16, completed_profile=$17 WHERE id=$18 RETURNING *', [
@@ -79,7 +81,7 @@ const updateUser = (req, res, next) => {
           twitter,
           instagram,
           pinterest,
-          completed_profile,
+          isComplete,
           id
         ])
       ]
@@ -107,7 +109,7 @@ const updateUser = (req, res, next) => {
   const joinResults = () => {
     return db.tx(transaction => {
       const queries = [
-        transaction.one('SELECT first_name, last_name, birth_date, hometown, profile_picture, introduction, has_kids, has_pets, number_of_kids, about_pets, is_service_member, current_station, facebook, twitter, instagram, pinterest, completed_profile from ohhi_user WHERE id=$1', [id]),
+        transaction.one('SELECT id, first_name, last_name, birth_date, hometown, profile_picture, introduction, has_kids, has_pets, number_of_kids, about_pets, is_service_member, current_station, facebook, twitter, instagram, pinterest, completed_profile from ohhi_user WHERE id=$1', [id]),
         transaction.query('SELECT kids_age_label FROM ohhi_user_kids_age LEFT JOIN ohhi_kids_age ON ohhi_kids_age.id = ohhi_user_kids_age.kids_age_id where ohhi_user_kids_age.user_id=$1', [id]),
         transaction.query('SELECT activity_label FROM ohhi_user_activity LEFT JOIN ohhi_activity ON ohhi_activity.id = ohhi_user_activity.activity_id where ohhi_user_activity.user_id=$1', [id])
       ]
@@ -125,6 +127,14 @@ const updateUser = (req, res, next) => {
     })
 
     res.status(200).json({ message: 'User updated successfully!', success: true, data: user })
+  }
+
+  const completedProfile = (user) => {
+    const required = ['first_name', 'last_name', 'birth_date', 'hometown', 'introduction']
+
+    return every(required, item => {
+      return item in user
+    })
   }
 
   return Promise
