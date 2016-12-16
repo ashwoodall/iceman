f9ecfa38d4b518f64039a10f77c4745008a59aa4
Brandon Clark <yhbrandon@gmail.com>
2016-12-15 23:03:42 -0600

    Shouldn't return yourself in the people list

--- src/modules/user/methods/getAllbyStation.js
+++ src/modules/user/methods/getAllbyStation.js
@@ -2,13 +2,14 @@ import db from '../../../core/db'
 
 const getAllbyStation = (req, res, next) => {
   const { station } = req.params
+  const { id } = req.user
 
   db.many('SELECT first_name, last_name, id, birth_date, completed_profile, profile_picture, introduction FROM ohhi_user WHERE current_station=$1', [station])
     .then(users => {
       let people = []
 
       users.map(user => {
-        if (user.completed_profile) people.push(user)
+        if (user.completed_profile || user.id !== id) people.push(user)
       })
 
       res.status(200).json({ message: 'Users found!', success: true, data: people })
