52d3d94ad144a690ae4da1b5da04b1c327111629
Brandon Clark <yhbrandon@gmail.com>
2016-12-16 00:12:17 -0600

    let return some things

--- src/modules/user/methods/disableUser.js
+++ src/modules/user/methods/disableUser.js
@@ -3,8 +3,12 @@ import db from '../../../core/db'
 const disableUser = (req, res, next) => {
   const { id } = req.user
 
-  db.one('UPDATE ohhi_user SET disabled=$1 WHERE ID=$2 RETURNING id', [true, id])
-    .then(id => res.status(200).json({ message: 'Account successfully disabled!', success: true }))
+  db.one('UPDATE ohhi_user SET disabled=$1 WHERE ID=$2 RETURNING *', [true, id])
+    .then(user => {
+      delete user.password
+
+      res.status(200).json({ message: 'Account successfully disabled!', success: true, data: user })
+    })
     .catch(error => {
       res.status(400).json({ success: false, message: 'Cannot disable account!' })
 
--- src/modules/user/methods/enableUser.js
+++ src/modules/user/methods/enableUser.js
@@ -4,9 +4,13 @@ const deleteUser = (req, res, next) => {
   const { id } = req.user
 
   db.one('UPDATE ohhi_user SET disabled=$1 WHERE ID=$2 RETURNING id', [false, id])
-    .then(id => res.status(200).json({ message: 'Account successfully deleted!', success: true }))
+    .then(user => {
+      delete user.password
+
+      res.status(200).json({ message: 'Account successfully enabled!', success: true, data: user })
+    })
     .catch(error => {
-      res.status(400).json({ success: false, message: 'Cannot delete account!' })
+      res.status(400).json({ success: false, message: 'Cannot enable account!' })
 
       return next(error)
     })
