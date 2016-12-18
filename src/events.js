3fcd450000a33f5fc75b9e79b02ccf2c76bcbce5
Brandon Clark <yhbrandon@gmail.com>
2016-12-18 16:48:03 -0600

    adding users to map

--- src/events.js
+++ src/events.js
@@ -1,9 +1,14 @@
+let clients = {}
+
 const events = (io) => {
   io.on('connection', (socket) => {
-    console.log('user connected')
 
-    io.sockets.sockets.map(function(e) {
-      console.log(e)
+    socket.on('user-connected', user => {
+      console.log('user connected')
+
+      clients[user] = socket.id
+
+      console.log(clients)
     })
 
     socket.emit('connected', { connected: true })
@@ -12,16 +17,26 @@ const events = (io) => {
       socket.emit('receive socket', socket.id)
     })
 
-    socket.on('join conversation', (conversation) => {
+    socket.on('join conversation', conversation => {
       socket.join(conversation)
     })
 
-    socket.on('new message', (message) => {
+    socket.on('new message', message => {
       io.to(message.convo_id).emit('new socket message', message)
     })
 
-    socket.on('new conversation', (conversation) => {
+    socket.on('new conversation', conversation => {
       socket.broadcast.emit('new socket conversation', conversation)
+    })
+
+    socket.on('disconnect', function() {
+      for (var user in clients) {
+        if (clients[user] === socket.id) {
+          delete clients[name]
+
+          break
+        }
+      } 
     })
   })
 }
