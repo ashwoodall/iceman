8f86bbc829bb3765664da86f9c708794b6269f4d
Matt Phillips <matthewcannonphillips@gmail.com>
2016-12-18 15:19:23 -0600

    Sort by ASC on messages

--- src/modules/message/methods/getAllMessages.js
+++ src/modules/message/methods/getAllMessages.js
@@ -3,7 +3,7 @@ import db from '../../../core/db'
 const getAllMessages = (req, res, next) => {
   const { conversationId } = req.params
 
-  return db.many('SELECT * FROM ohhi_message WHERE convo_id=$1 AND is_deleted = false ORDER BY created_at DESC', [conversationId])
+  return db.many('SELECT * FROM ohhi_message WHERE convo_id=$1 AND is_deleted = false ORDER BY created_at ASC', [conversationId])
     .then(result => res.status(200).json({ message: 'Messages found!', success: true, data: result }))
     .catch(error => {
       res.status(400).json({ message: 'Cannot find messages!', success: false })
