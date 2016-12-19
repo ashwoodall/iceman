cade7d1e15eda8ef839dbea2fd1f170ee59e19f1
Matt Phillips <matthewcannonphillips@gmail.com>
2016-12-18 19:51:27 -0600

    Send email on registration and consolidate secrets into one gitignored config

--- src/modules/auth/methods/register.js
+++ src/modules/auth/methods/register.js
@@ -1,7 +1,10 @@
 import Promise from 'bluebird'
 import { hash, genSalt } from 'bcrypt-nodejs'
+import postmark from 'postmark'
+import CryptoJS from 'crypto-js'
 
 import db from '../../../core/db'
+import secrets from '../../../../secrets'
 
 const bcryptHash = Promise.promisify(hash)
 const bcryptSalt = Promise.promisify(genSalt)
@@ -11,7 +14,21 @@ const register = (req, res, next) => {
 
   return bcryptSalt(10)
     .then(salt => bcryptHash(password, salt, null))
-    .then(hashedPassword => db.none('INSERT INTO ohhi_user(email, password, current_station) values($1, $2, $3)', [email, hashedPassword, current_station]))
+    .then(hashedPassword => db.one('INSERT INTO ohhi_user(email, password, current_station) values($1, $2, $3) RETURNING id', [email, hashedPassword, current_station]))
+      .then((record) => {
+        var client = new postmark.Client(secrets.postmark.key)
+
+        const encryptedId = CryptoJS.AES.encrypt(record.id.toString(), secrets.crypto.idSalt)
+
+        client.sendEmailWithTemplate({
+          From: 'hi@oh-hi.us',
+          TemplateId: secrets.postmark.templateId,
+          To: email,
+          TemplateModel: {
+            activationUrl: 'http://www.app.oh-hi.us/activate/' + encryptedId.toString()
+          }
+        })
+      })
     .then(() => res.status(200).json({ message: 'Registration successful!', success: true }))
     .catch(error => {
       res.status(400).json({ success: false, message: 'That email address is already associated with an account. You may log in or enter a different email address' })
