6a402eb4ae14c3228cd7dd5a6e3e1bc9ef1b7cba
Brandon Clark <yhbrandon@gmail.com>
2016-12-20 18:18:31 -0600

    testing auth

--- src/modules/user/routes.js
+++ src/modules/user/routes.js
@@ -1,7 +1,7 @@
 import { deleteUser, disableUser, enableUser, getAllbyStation, getById, getByToken, updateUser, getCredentials } from './methods'
 
 const routes = (app, passport) => {
-  app.get('/user/by/token', passport.authenticate('jwt', { session: false }), getByToken)
+  app.get('/user/by/token', getByToken)
   app.get('/user/:userId', passport.authenticate('jwt', { session: false }), getById)
   app.put('/user', passport.authenticate('jwt', { session: false }), updateUser)
   app.put('/user/:userId/disable', passport.authenticate('jwt', { session: false }), disableUser)
