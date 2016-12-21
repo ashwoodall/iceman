cb017495bdbd8232fb9e6858f088e0392f6b0a0e
Brandon Clark <yhbrandon@gmail.com>
2016-12-20 18:02:20 -0600

    trying something

--- src/modules/user/methods/getAllbyStation.js
+++ src/modules/user/methods/getAllbyStation.js
@@ -4,7 +4,7 @@ const getAllbyStation = (req, res, next) => {
   const { station } = req.params
   const { id } = req.user
 
-  db.any('SELECT first_name, last_name, id, birth_date, completed_profile, profile_picture, introduction FROM ohhi_user WHERE current_station=$1 AND is_activated = true AND completed_profile = true AND disabled = false', [station])
+  db.any('SELECT first_name, last_name, id, birth_date, completed_profile, profile_picture, introduction FROM ohhi_user WHERE current_station=$1 AND is_activated=$2 AND completed_profile=$3 AND disabled=$4', [station, true, true, false])
     .then(users => {
       let people = []
 
